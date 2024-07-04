5937a35a130612666ce9fb68034e6f7774e92746
Andre <andre.erlinghagen@schleupen.de>
2024-07-04 10:03:05 +0200

    Send/Receive-Options -> set prop setter to init

--- BusinessAdapter/Configuration/ReceiveOptions.cs
+++ BusinessAdapter/Configuration/ReceiveOptions.cs
@@ -2,9 +2,9 @@ namespace Schleupen.AS4.BusinessAdapter.Configuration;
 
 public record ReceiveOptions
 {
-	public int RetryCount { get; set; } = 3;
+	public int RetryCount { get; init; } = 3;
 
-	public int MessageLimitCount { get; set; } = 1000;
+	public int MessageLimitCount { get; init; } = 1000;
 
-	public string Directory { get; set; }
+	public string Directory { get; init; } = default!;
 }
