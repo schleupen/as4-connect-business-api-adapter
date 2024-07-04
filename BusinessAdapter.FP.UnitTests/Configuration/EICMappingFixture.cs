a4f7131c6923792d4b24d87debc3f8ad6b247fcd
Andre <andre.erlinghagen@schleupen.de>
2024-07-04 09:06:21 +0200

    refactor to Fixture with TestData

---
+++ BusinessAdapter.FP.UnitTests/Configuration/EICMappingFixture.cs
@@ -0,0 +1,59 @@
+﻿namespace Schleupen.AS4.BusinessAdapter.FP.UnitTests.Configuration;
+
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using Schleupen.AS4.BusinessAdapter.API;
+using Schleupen.AS4.BusinessAdapter.FP.Configuration;
+
+public partial class EICMappingTest
+{
+	private Fixture fixture = new();
+
+	[SetUp]
+	public void Setup()
+	{
+		fixture = new Fixture();
+	}
+
+	[TearDown]
+	public void Dispose()
+	{
+		fixture = null!;
+	}
+
+	private sealed class Fixture
+	{
+		public TestData Data { get; } = new();
+
+		public EICMapping CreateSimpleEicMapping()
+		{
+			EICMapping mapping = new EICMapping()
+			{
+				{ Data.Eic1.Code, Data.Party1 },
+				{ Data.Eic2.Code, Data.Party2 }
+			};
+			return mapping;
+		}
+
+		public EICMapping LoadFromAppSettings()
+		{
+			EICMapping mapping = new EICMapping();
+			var config = new ConfigurationBuilder()
+				.AddJsonFile("appsettings.unittests.json")
+				.Build();
+
+			var options = config.GetSection(EICMapping.SectionName);
+			options.Bind(mapping);
+
+			return mapping;
+		}
+	}
+
+	private sealed class TestData
+	{
+		public readonly EIC Eic1 = new EIC("eic1");
+		public readonly EIC Eic2 = new EIC("eic2");
+		public readonly Party Party1 = new Party("id1", "type");
+		public readonly Party Party2 = new Party("id2", "type");
+	}
+}
--- BusinessAdapter.FP.UnitTests/Configuration/EICMappingTest.cs
+++ BusinessAdapter.FP.UnitTests/Configuration/EICMappingTest.cs
@@ -1,27 +1,25 @@
 ﻿namespace Schleupen.AS4.BusinessAdapter.FP.UnitTests.Configuration;
 
-using Microsoft.Extensions.Configuration;
 using NUnit.Framework;
 using Schleupen.AS4.BusinessAdapter.API;
-using Schleupen.AS4.BusinessAdapter.FP.Configuration;
 
-public class EICMappingTest
+public partial class EICMappingTest
 {
 	[Test]
 	public void GetEICOrDefault_Mapped_ShouldReturnEic()
 	{
-		var mapping = CreateSimpleEicMapping();
+		var mapping = fixture.CreateSimpleEicMapping();
 
-		var result = mapping.GetEICOrDefault(new Party("id1", "type"));
+		var result = mapping.GetEICOrDefault(fixture.Data.Party1);
 
 		Assert.That(result, Is.Not.Null);
-		Assert.That(result, Is.EqualTo(new EIC("eic1")));
+		Assert.That(result, Is.EqualTo(fixture.Data.Eic1));
 	}
 
 	[Test]
 	public void GetEICOrDefault_Unmapped_ShouldReturnNull()
 	{
-		var mapping = CreateSimpleEicMapping();
+		var mapping = fixture.CreateSimpleEicMapping();
 
 		var result = mapping.GetEICOrDefault(new Party("na", "na"));
 
@@ -31,17 +29,17 @@ public class EICMappingTest
 	[Test]
 	public void GetPartyOrDefault_Mapped_ShouldReturnParty()
 	{
-		var mapping = CreateSimpleEicMapping();
+		var mapping = fixture.CreateSimpleEicMapping();
 
-		var result = mapping.GetPartyOrDefault(new EIC("eic1"));
+		var result = mapping.GetPartyOrDefault(fixture.Data.Eic1);
 
-		Assert.That(result, Is.EqualTo(new Party("id1", "type")));
+		Assert.That(result, Is.EqualTo(fixture.Data.Party1));
 	}
 
 	[Test]
 	public void GetPartyOrDefault_Unmapped_ShouldReturnNull()
 	{
-		var mapping = CreateSimpleEicMapping();
+		var mapping = fixture.CreateSimpleEicMapping();
 
 		var result = mapping.GetPartyOrDefault(new EIC("na"));
 
@@ -51,32 +49,9 @@ public class EICMappingTest
 	[Test]
 	public void GetPartyOrDefault_EICMappingFromJson_ShouldWork()
 	{
-		var eicMapping = this.LoadFromAppSettings();
+		var eicMapping = this.fixture.LoadFromAppSettings();
 
 		Assert.That(eicMapping, Is.Not.Null.Or.Empty);
 		Assert.That(eicMapping.GetPartyOrDefault(new EIC("10Y1001A1001A82H")), Is.EqualTo(new Party("1000000001", "BDEW")));
-	}
-
-	private EICMapping LoadFromAppSettings()
-	{
-		EICMapping mapping = new EICMapping();
-		var config = new ConfigurationBuilder()
-			.AddJsonFile("appsettings.unittests.json")
-			.Build();
-
-		var options = config.GetSection(EICMapping.SectionName);
-		options.Bind(mapping);
-
-		return mapping;
-	}
-
-	private static EICMapping CreateSimpleEicMapping()
-	{
-		EICMapping mapping = new EICMapping()
-		{
-			{ "eic1", new Party("id1", "type") },
-			{ "eic2", new Party("id2", "type") }
-		};
-		return mapping;
 	}
 }
