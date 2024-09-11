8712a4c885b65e4f6ab49c30df911af7a04a5694
Erlinghagen, Andre <andre.erlinghagen@schleupen.de>
2024-09-11 14:25:33 +0200

    fix curl

--- SetCommitStatus.ps1
+++ SetCommitStatus.ps1
@@ -6,7 +6,7 @@ $repo = "as4-connect-business-api-adapter"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user, $password)))
 
-$headers = @{Authorization = "Basic $base64AuthInfo" }
+$headers = @{ Authorization = "Basic $base64AuthInfo" }
 
 $url = "https://api.github.com/repos/schleupen/$($repo)/statuses/$($sha)"
 Write-Host $url
@@ -14,5 +14,4 @@ curl.exe -XPOST -H "Authorization: token $password" $url -d "{
       \"state\": \"pending\",
       \"target_url\": \"https://jenkins",
       \"description\": \"The build is pending!\"
-     '
     }"
