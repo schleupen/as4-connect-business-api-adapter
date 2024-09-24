b1fc9b4d1814939261edd8e7f9ebc1b43066c2b4
jbr-Schleupen <jasper.brauer@schleupen.de>
2024-09-24 12:48:34 +0200

    Fixed MP integrative tests

--- BusinessAdapter.MP.IntegrativeTests/SendAndReceiveTests.cs
+++ BusinessAdapter.MP.IntegrativeTests/SendAndReceiveTests.cs
@@ -27,16 +27,15 @@ public sealed partial class SendAndReceiveTests : IDisposable
 	    // we expect files to be downloaded in the configured receive directory
 	    Assert.That(fixture.CheckReceiveFileDirIsEmpty(), Is.False);
 	}
-
+    
     [Test]
-    public async Task SendAndReceiveTests_SendOfValidFiles_WithoutCert()
+    public async Task SendAndReceiveTests_ReceiveOfValidFiles_WithoutCert()
     {
 	    fixture.CreateDefaultAppSettings("9912345000003");
 	    var configFileOption = new FileInfo(fixture.AppSettingsPath);
 
 	    var exception = Assert.ThrowsAsync<AggregateException> (() => this.fixture.Receive(configFileOption));
 	    Assert.That(exception.InnerExceptions[0].Message, Is.EqualTo("No certificate found for the market partner with identification number 9912345000003."));
-
 
 	    // we expect the file to be there after a failing send
 	    Assert.That(fixture.CheckSendFile(), Is.True);
