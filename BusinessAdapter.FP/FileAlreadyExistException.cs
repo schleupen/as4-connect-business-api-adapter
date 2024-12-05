fa66cb02e39583b5f674fc7c1a993f565b2f8e52
jbr-Schleupen <jasper.brauer@schleupen.de>
2024-12-05 16:22:35 +0100

    Adjusted retries and changed override of files to warning in FP

---
+++ BusinessAdapter.FP/FileAlreadyExistException.cs
@@ -0,0 +1,11 @@
+﻿namespace Schleupen.AS4.BusinessAdapter.FP;
+
+[Serializable]
+public class FileAlreadyExistException(string fileName, string messageId)
+    : Exception($"File with the name {fileName} already exist for message {messageId}")
+{
+    
+    public string FileName { get; } = fileName;
+
+    public string MessageId { get; } = messageId;
+}
